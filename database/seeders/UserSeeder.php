b8d279f8e1ef1fa886a3b3acf7d63d9b8a68263f
Harshit <hc.web1992@gmail.com>
2024-04-24 18:59:31 +0530

    First Commit From Office

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $json = File::get('database\json\users.json');
+        $users = json_decode($json);
+
+        foreach ($users as $user) {
+            User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'age' => $user->age,
+                'city' => $user->city,
+            ]);
+        }
+    }
+}
